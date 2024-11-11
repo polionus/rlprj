d289df7377a407331bebffe105ac41ce46d1c902
polionus <ashrafi2@ualberta.ca>
2024-11-11 16:23:09 -0700

    added the config file

--- experiments/config.py
+++ experiments/config.py
@@ -1,6 +1,18 @@
 settings = {
-    "",
-    "",
-    "",
+    "n_timsteps": 500_000,
+    "policy_kwargs": dict(net_arch=[64, 64, 64]),
+    "seeds":100,
+    "start_delta_exponent":-15,
+    "end_delta_exponent":0,
+    "num_deltas":15,
+    "deltas":,
+    "training_delta":1e-10,
+    "checker_delta":1e-10,
+    "desc_color":"\033[92m",
+    "reset_color":"\033[0m",
+    "path_to_google_drive":"path/to/desktop/google/drive/RL Project (Fall 2024)/Results/CartPole_Single",
+
+
+
 
 }
