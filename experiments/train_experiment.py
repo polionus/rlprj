579a981653b5943679ce3b70eb8bdbaec9b6fb0a
polionus <ashrafi2@ualberta.ca>
2024-11-12 15:52:52 -0700

    made some changes to make add random policy test

--- experiments/train_experiment.py
+++ experiments/train_experiment.py
@@ -8,9 +8,16 @@ n_timesteps = settings["n_timesteps"]
 policy_kwargs = settings["policy_kwargs"]
 training_delta = settings["training_delta"]
 
+gym.register(
+    id="CustomCartPole-v0",
+    entry_point="custom_cartpole:CustomCartPoleEnv",
+)
+
 def train_model():
     env = gym.make("CustomCartPole-v0", dt_multip = training_delta)
     model = PPO("MlpPolicy", env, policy_kwargs = policy_kwargs, verbose=1)
     model.learn(total_timesteps=n_timesteps)
     model.save("ppo_cartpole")
     env.close()
+
+train_model()
